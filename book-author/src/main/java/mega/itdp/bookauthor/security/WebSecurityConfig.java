7e05734b02001a0b7162f36c032353f9768593ed
Bylvin <56466617+Bylvin@users.noreply.github.com>
2022-10-16 17:48:23 +0700

    Add pom security & CORS

---
+++ book-author/src/main/java/mega/itdp/bookauthor/security/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package mega.itdp.bookauthor.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        .authorizeHttpRequests((requests) -> requests
+                .anyRequest().permitAll()
+                )
+        .cors(withDefaults())
+        .csrf().disable()
+        .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200"));
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
